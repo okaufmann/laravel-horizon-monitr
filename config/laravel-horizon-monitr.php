50c76967b304a1877bf42ac2db6a9afc35c47fa2
Oliver Kaufmann <okaufmann91@gmail.com>
2021-04-18 15:43:40 +0000

    Fix code style 💄
    [ci skip] [skip ci]

--- config/laravel-horizon-monitr.php
+++ config/laravel-horizon-monitr.php
@@ -4,5 +4,5 @@ return [
     'monitr' => [
         'url' => env('LARAVEL_HORIZON_MONITR_URL', 'https://monitr.mighty-code.com'),
         'token' => env('LARAVEL_HORIZON_MONITR_TOKEN'),
-    ]
+    ],
 ];
